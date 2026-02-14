ec83bd91d211d65affa4ee0c64e8025ba674d1d1
John McBride <john@papercompute.com>
2026-02-14 15:19:11 -0500

    feat: go mod vendor and nix flake build (#2)
    Signed-off-by: John McBride <john@papercompute.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Tapes - Development environment";
+  description = "stereosd - StereOS control plane daemon";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -7,26 +7,87 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    {
+      overlays.default = final: prev: {
+        stereosd = final.buildGoModule {
+          pname = "stereosd";
+          version = self.shortRev or "dev";
+          src = final.lib.cleanSource self;
+          vendorHash = null;
+          ldflags = [
+            "-s" "-w"
+            "-X github.com/papercomputeco/stereosd/pkg/version.Version=${self.shortRev or "dev"}"
+          ];
+          env.CGO_ENABLED = 0;
+
+          # Tests require system-level capabilities (vsock, mount, shutdown)
+          # that are unavailable in the Nix build sandbox.
+          doCheck = false;
+        };
+      };
+
+      # NixOS module systemd service definition
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let cfg = config.services.stereosd; in
+        {
+          options.services.stereosd = {
+            enable = lib.mkEnableOption "stereosd daemon";
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+              description = "The stereosd package to use";
+            };
+            extraArgs = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [];
+              description = "Additional command-line arguments";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.stereosd = {
+              description = "StereOS Control Plane Daemon";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              serviceConfig = {
+                ExecStart = "${cfg.package}/bin/stereosd ${lib.escapeShellArgs cfg.extraArgs}";
+                Restart = "always";
+                DynamicUser = true;
+                StateDirectory = "stereosd";
+              };
+            };
+          };
+        };
+    }
+    //
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
       in
       {
+        packages = {
+          default = pkgs.stereosd;
+          stereosd = pkgs.stereosd;
+        };
+
         devShells.default = pkgs.mkShell {
-          buildInputs = [
+          buildInputs = with pkgs; [
             # Go toolchain
-            pkgs.go_1_25
-            pkgs.gotools
+            go_1_25
+            gotools
 
             # Build tools
-            pkgs.gnumake
+            gnumake
 
             # Test tools
-            pkgs.hurl
+            hurl
           ];
 
           shellHook = ''
-            echo "Tapes development environment"
+            echo "stereosd development environment"
             echo ""
             echo "Go version: $(go version)"
             echo ""
@@ -37,4 +98,3 @@
       }
     );
 }
-
